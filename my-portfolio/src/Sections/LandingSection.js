837995166b1ea7916dba3aabef06e67ebddf89a0
Jorge <jorgesilva1892@gmail.com>
2021-06-04 17:58:59 -0700

    icons are loading in, need to play with the animation and sizing

--- my-portfolio/src/Sections/LandingSection.js
+++ my-portfolio/src/Sections/LandingSection.js
@@ -1,7 +1,26 @@
-import React from "react";
+import StarIcon from "../components/StarIcon";
+import CompassIcon from "../components/CompassIcon";
+import "../_morphing.scss";
 
 const LandingSection = () => {
-  return <div></div>;
+  return (
+    <div className="landing-tile">
+      <div className="top-star-controller">
+        <StarIcon />
+        <StarIcon />
+      </div>
+      <StarIcon />
+      <div className="greating-compass-container">
+        <h2 className="landing-greeting">Welcome 👋🏼 I’m Jorge</h2>
+        <CompassIcon />
+        <div class="morphing">
+          <div class="word">a USN Veteran</div>
+          <div class="word">UX/UI Designer &</div>
+          <div class="word">Fronted Developer</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LandingSection;
