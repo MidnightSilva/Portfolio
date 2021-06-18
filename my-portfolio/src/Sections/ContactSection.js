e151922968ce4ba1c53bb0808b17c195414743f9
Jorge <jorgesilva1892@gmail.com>
2021-06-18 12:25:15 -0700

    adding media queries for 576px  and 992px

--- my-portfolio/src/Sections/ContactSection.js
+++ my-portfolio/src/Sections/ContactSection.js
@@ -3,12 +3,14 @@ import MyForm from "../components/Myform.js";
 const ContactSection = () => {
   return (
     <div className="formpage" id="form">
-      <h2 className="heading5">Contact me</h2>
-      <div className="backdrop"></div>
-      <p className="subheading5">
-        Leave a message and I'll get back to you as soon as I can. Looking
-        forward to hearing from you.
-      </p>
+      <div className="backdrop">
+        <h2 className="heading5">Contact me</h2>
+        <p className="subheading5">
+          Leave a message and I'll get back to you as soon as I can. Looking
+          forward to hearing from you.
+        </p>
+      </div>
+
       <MyForm />
     </div>
   );
