5dec52bbb9ba4f7ae5af3ac7985a4040a95d0d20
Jorge <jorgesilva1892@gmail.com>
2021-06-19 16:30:33 -0700

    adjusting media queries for site

--- my-portfolio/src/Sections/ContactSection.js
+++ my-portfolio/src/Sections/ContactSection.js
@@ -3,14 +3,12 @@ import MyForm from "../components/Myform.js";
 const ContactSection = () => {
   return (
     <div className="formpage" id="form">
-      <div className="backdrop">
+    
         <h2 className="heading5">Contact me</h2>
         <p className="subheading5">
           Leave a message and I'll get back to you as soon as I can. Looking
           forward to hearing from you.
         </p>
-      </div>
-
       <MyForm />
     </div>
   );
