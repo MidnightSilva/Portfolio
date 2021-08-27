e952c21e7b829bb0b9e3f2b69bb31829d8d8a9cf
Jorge <jorgesilva1892@gmail.com>
2021-08-26 18:43:32 -0700

    starting new layout for portfoilo

--- my-portfolio/src/pages/HomePage.js
+++ my-portfolio/src/pages/HomePage.js
@@ -1,17 +1,14 @@
-import Skills from "../Sections/Skills";
+
 import LandingSection from "../Sections/LandingSection";
 import AboutMeSection from "../Sections/AboutMeSection";
 import ProjectSection from "../Sections/ProjectSection";
-import ContactSection from "../Sections/ContactSection";
 
 const HomePage = () => {
   return (
     <div className="Main-Sec">
       <LandingSection />
       <AboutMeSection />
-      <Skills />
       <ProjectSection />
-      <ContactSection />
     </div>
   );
 };
