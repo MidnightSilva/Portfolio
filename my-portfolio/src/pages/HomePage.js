d9b9f75736d2180ecb2604a6e4ee82e575738cd5
Jorge <jorgesilva1892@gmail.com>
2021-08-26 18:58:24 -0700

    comented out the about me and the project section to foucs on the animation for the landing section of the new website

--- my-portfolio/src/pages/HomePage.js
+++ my-portfolio/src/pages/HomePage.js
@@ -1,14 +1,14 @@
 
 import LandingSection from "../Sections/LandingSection";
-import AboutMeSection from "../Sections/AboutMeSection";
-import ProjectSection from "../Sections/ProjectSection";
+// import AboutMeSection from "../Sections/AboutMeSection";
+// import ProjectSection from "../Sections/ProjectSection";
 
 const HomePage = () => {
   return (
     <div className="Main-Sec">
       <LandingSection />
-      <AboutMeSection />
-      <ProjectSection />
+      {/* <AboutMeSection />
+      <ProjectSection /> */}
     </div>
   );
 };
