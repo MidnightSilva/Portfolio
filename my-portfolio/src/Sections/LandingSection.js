c41da930b9f0d8977197fa2a3cd83a1ab5eec200
Jorge <jorgesilva1892@gmail.com>
2021-09-23 18:49:45 -0700

    adjusted the hero

--- my-portfolio/src/Sections/LandingSection.js
+++ my-portfolio/src/Sections/LandingSection.js
@@ -1,21 +1,18 @@
-// import StarIcon from "../components/StarIcon";
-import CompassIcon from "../components/CompassIcon";
+import CompassIcon from "../components/CompassIcon"
+import OuterCompass from "../components/OuterCompass"
 
 const LandingSection = () => {
   return (
     <div className="landing-tile">
-      <div className="top-star-controller">
-        {/* <StarIcon />
-        <StarIcon /> */}
-      </div>
-      {/* <StarIcon /> */}
-      <div className="top-star-controller-2">
-        {/* <StarIcon />
-        <StarIcon /> */}
-      </div>
       <div className="greating-compass-container">
         <h2 className="landing-greeting">Welcome 👋🏼 I’m Jorge</h2>
-        <CompassIcon />
+        <div className="compass-container">
+          <OuterCompass>
+            <CompassIcon />
+          </OuterCompass>
+          <CompassIcon />
+        </div>
+
         <div class="morphing">
           <div class="word">a USN Veteran</div>
           <div class="word">UX/UI Designer &</div>
