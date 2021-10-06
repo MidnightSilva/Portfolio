d8c9377333ccb0e4919d64805cee9188aa32810a
Jorge <jorgesilva1892@gmail.com>
2021-10-06 09:10:23 -0700

    adjusting landing section

--- my-portfolio/src/Sections/LandingSection.js
+++ my-portfolio/src/Sections/LandingSection.js
@@ -1,14 +1,39 @@
-
+import { motion } from "framer-motion";
 
 const LandingSection = () => {
+   
+         const item = {
+           hidden: {
+             opacity: [0,1],
+             transition: { duration: 1 },
+           },
+         };
+     const item2 = {
+       hidden: {
+        opacity: [0],
+         transition: { duration: 1 },
+       },
+     };
   return (
     <div className="landing-tile">
-        <h2 className="landing-greeting">Welcome 👋🏼 I’m Jorge</h2>
-        <div class="morphing">
+      <motion.h2 className="landing-greeting">
+        Welcome to my portfoilo{" "}
+        <motion.span className='hand'>
+          👋🏼
+        </motion.span>
+      </motion.h2>
+
+      <p className="word">
+        My name is Jorge. I am a
+        <motion.span className="highlight"> USN Veteran,</motion.span>
+        <motion.span className="highlight">Full Stack dev,</motion.span> with
+        some <motion.span className="highlight">UXUI</motion.span> Experience.
+      </p>
+      {/* <div class="morphing">
           <div class="word">a USN Veteran</div>
           <div class="word">UX/UI Designer &</div>
           <div class="word">Fronted Developer</div>
-        </div>
+        </div> */}
       {/* <div class="arrow">
         <span></span>
         <span></span>
