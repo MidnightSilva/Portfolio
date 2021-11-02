2b1b4032583386a1d8f7ff71dc4b597069d04ff0
Jorge <jorgesilva1892@gmail.com>
2021-11-02 11:48:50 -0700

    adding bottom forest image

--- my-portfolio/src/Sections/ContactSection.js
+++ my-portfolio/src/Sections/ContactSection.js
@@ -4,7 +4,7 @@ import MyForm from "../components/Myform.js";
 const ContactSection = () => {
 
   return (
-    <div>
+    <div className='form-img'>
     <div className="formpage" id="form">
       <h2 className="heading5">Contact me</h2>
       <p className="subheading5">
