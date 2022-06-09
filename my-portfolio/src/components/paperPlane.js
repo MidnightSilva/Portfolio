2fee5d7108336ff94bbc7bae4929ec9f5fd5c61a
Jorge <jorgesilva1892@gmail.com>
2022-06-08 18:22:35 -0700

    image split and slight animation

--- my-portfolio/src/components/paperPlane.js
+++ my-portfolio/src/components/paperPlane.js
@@ -1,19 +1,26 @@
-import paperplane from "../assets/Images/paperplanewithtrail.jpg";
+import paperplane from "../assets/Images/planejpg.jpg";
+import Trail from "../assets/Images/trail.jpg";
 
  const PaperPlane = (props) => {
 
 
  
   return (
-    <>
+    <div className="plane-container">
       {" "}
+      <img
+        className="trail"
+        alt="paper plane"
+        src={Trail}
+        onClick={props}
+      />
       <img
         className="message-plane"
         alt="paper plane"
         src={paperplane}
         onClick={props}
       />
-    </>
+    </div>
   );
 }
 
